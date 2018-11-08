efad3d3e2a768007d6800543b785fde17c0946b9
cg <chuganghong@qq.com>
2018-11-08 18:15:07 +0800

    理解 快速排序 中的 partion 中的外层循环中交换值的选定。 使用的是尝试方法来理解

--- Sort/QuickSort.php
+++ Sort/QuickSort.php
@@ -61,8 +61,21 @@ class QuickSort extends BaseSort
             $this->exch($arr, $i, $j);
         }
 
+        /**
+         * 突然理解不了，这里为何是 交换 $start 和 $j，而不是交换 $start 和 $i 。
+         * while(true){}终止的条件是 $i >= $j，$arr[$i] >= $v, $arr[$j] <= $v
+         * 1> $i > $j
+         * * A> 终止条件的前一步是, $i-1 和 $j+1，二者已经遍历完毕数组。
+         * * B> 从$start到$i-1位置的元素，都小于$start位置的元素；从$j+1到$end位置的元素，都大于$start的元素。
+         * * C> 终止时，$arr[$i] >= $v, $arr[$j] <= $v
+         * * * a> 若将v与$arr[$i]互换，那么，$start到$i-1位置的元素都小于$arr[$i](==$v)，但$arr[$start]大于$arr[$i](==$v)
+         * * * a> $i+1到$end位置的元素，都大于$arr[$i](==$v)
+         * * * a> i之前的元素，不全小于 $arr[$i](==$v)，没有达到"将数组调整为基本有序"的效果。
+         * * * b> 若将v与$arr[$j]互换，那么，从$j+1到$end的元素，都大于$arr[$j](==$v)，$arr[$start](==$arr[$j]) <= $v，数组变为
+         * * * b> 基本有序：$j之前的都小于$arr[$j](==$v)，$j之后的都大于$arr[$j](==$v)
+         * 2> $i == $j，用i或j与start交换效果相同
+         */
         $this->exch($arr, $start, $j);
-
         return $j;
     }
 }
