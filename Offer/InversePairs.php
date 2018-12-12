1758d26eaa5867681118bd940233a6e3d7dd115e
cg <chuganghong@qq.com>
2018-12-12 20:32:41 +0800

    数组中的逆序对

--- Offer/InversePairs.php
+++ Offer/InversePairs.php
@@ -31,9 +31,65 @@ class InversePairs
 
         }
 
-        var_dump($num);
+//        var_dump($num);
 
         return $num % self::BASE;
+    }
+
+    public function run2(array $data): int
+    {
+        $copy = $data;
+        $length = count($data);
+        $num = $this->inversePairesCore($data,$copy, 0,  $length-1);
+        return $num % self::BASE;
+    }
+
+
+    private function inversePairesCore(
+        array &$data,
+        array &$copy,
+        int $start,
+        int $end
+    ): int
+    {
+        // 基准条件
+        if($start == $end){
+
+            $copy[$start] = $data[$end];
+            return 0;
+        }
+
+        $length = intval(floor(($end - $start) / 2));
+        $left = $this->inversePairesCore($copy, $data, $start, $start + $length);
+        $right = $this->inversePairesCore($copy, $data, $start + $length + 1, $end);
+
+        $count = 0;
+        $i = $start + $length;
+        $j = $end;
+        // $copy的key用$end或$indexCopy都可以
+        $indexCopy = $end;
+
+        while($i >= $start && $j >= $start + $length + 1){
+            if($data[$i] > $data[$j]){
+                /**
+                 * 不能用$end代替$j，因为统计的数量是$j（包括$j）之前的元素个数
+                 */
+                $count += $j - ($start + $length + 1) + 1;
+                $copy[$indexCopy--] = $data[$i--];
+            }else{
+                $copy[$indexCopy--] = $data[$j--];
+            }
+        }
+
+        for( ; $i >= $start; $i--){
+            $copy[$indexCopy--] = $data[$i];
+        }
+
+        for( ; $j >= $start + $length + 1; $j--){
+            $copy[$indexCopy--] = $data[$j];
+        }
+
+        return $left + $count + $right;
     }
 
 
@@ -42,61 +98,22 @@ class InversePairs
 $class = new InversePairs();
 $arr = [1,2,3,4,5,6,7,0];
 $res = $class->run1($arr);
-var_dump($res);
+//var_dump($res);
 
 $arr = [7,5,6,4];
 $res = $class->run1($arr);
-var_dump($res);
+//var_dump($res);
 
-$arr = [364,637,341,406,747,995,234,971,571,219,993,407,416,366,315,301,601,650,418,355,460,505,360,965,516,648,727,667,465,849,455,181,486,149,588,233,144,174,557,67,746,550,474,162,268,142,463,221,882,576,604,739,288,569,256,936,275,401,497,82,935,983,583,523,697,478,147,795,380,973,958,115,773,870,259,655,446,863,735,784,3,671,433,630,425,930,64,266,235,187,284,665,874,80,45,848,38,811,267,575];
+$arr2 = [364,637,341,406,747,995,234,971,571,219,993,407,416,366,315,301,601,650,418,355,460,505,360,965,516,648,727,667,465,849,455,181,486,149,588,233,144,174,557,67,746,550,474,162,268,142,463,221,882,576,604,739,288,569,256,936,275,401,497,82,935,983,583,523,697,478,147,795,380,973,958,115,773,870,259,655,446,863,735,784,3,671,433,630,425,930,64,266,235,187,284,665,874,80,45,848,38,811,267,575];
 $res = $class->run1($arr);
+//var_dump($res);
+
+$arr = [7,5,6,4];
+$res = $class->run2($arr);
+var_dump($res);
+
+$res = $class->run2($arr2);
 var_dump($res);
 
 
-function InversePairs($data)
-{
-    // write code here
-    return mergeSort($data) % 1000000007;
-}
 
-function mergeSort(&$arr) {
-    $len = count($arr);
-    $sum = 0;
-    mSort($arr, 0, $len-1, $sum);
-    return $sum;
-}
-
-function mSort(&$arr, $left, $right, &$sum) {
-    if($left < $right) {
-        $center = ($left+$right) >> 1;
-        mSort($arr, $left, $center, $sum);
-        mSort($arr, $center+1, $right, $sum);
-        mergeArray($arr, $left, $center, $right, $sum);
-    }
-}
-
-function mergeArray(&$arr, $l, $m, $r, &$sum) {
-    $i = $l;
-    $j = $m+1;
-    while($i<=$m && $j<=$r) {
-        if($arr[$i] <= $arr[$j]) {
-            $temp[] = $arr[$i++];
-        } else {
-            $sum += $m - $i + 1;
-            $temp[] = $arr[$j++];
-        }
-    }
-    while($i <= $m) {
-        $temp[] = $arr[$i++];
-    }
-    while($j <= $r) {
-        $temp[] = $arr[$j++];
-    }
-    for($i=0, $len=count($temp); $i<$len; $i++) {
-        $arr[$l+$i] = $temp[$i];
-    }
-}
-
-$arr = [1,2,3,4,5,6,7,0];
-$res = InversePairs($arr);
-var_dump($res);
