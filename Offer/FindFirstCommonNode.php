6452b2329e9f913c8ba371d5e36c3e49ebb6ffc4
cg <chuganghong@qq.com>
2018-12-13 00:47:05 +0800

    输入两个链表，找出它们的第一个公共结点---使用辅助栈

--- Offer/FindFirstCommonNode.php
+++ Offer/FindFirstCommonNode.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace App\Offer;
 
+use App\Stack\Stack;
+
 /**
  * Class ListNode
  * @package App\Offer
@@ -10,7 +12,7 @@ namespace App\Offer;
  *
  * 1.很容易想到遍历的解法
  * * A>很简单的代码，却不能 bug free，写出了死循环。
- * 2.
+ * 2.使用辅助栈，修复 对空栈使用pop 错误，花了很多时间
  */
 
 class ListNode
@@ -18,7 +20,7 @@ class ListNode
     private $val;
     private $next = null;
 
-    public function __construct(int $val)
+    public function __construct(?int $val)
     {
         $this->val = $val;
     }
@@ -56,7 +58,7 @@ class FindFirstCommonNode
                 return null;
             }
             while($cur2->getVal()){
-                if($cur1->getVal() == $cur2->getVal()){
+                if($this->equal($cur1, $cur2)){
                     return $cur1;
                 }
                 $cur2 = $cur2->getNext();
@@ -73,6 +75,61 @@ class FindFirstCommonNode
 
         return null;
     }
+
+    public function run2(ListNode $pHead1, ListNode $pHead2): ?ListNode
+    {
+        // 先把链表放入栈
+        $stack1 = new \SplStack();
+        $stack2 = new \SplStack();
+        $cur1 = $pHead1;
+        $cur2 = $pHead2;
+        while($cur1 || $cur2){
+
+            if($cur1){
+                $stack1->push($cur1);
+                $cur1 = $cur1->getNext();
+
+            }
+
+            if($cur2){
+                $stack2->push($cur2);
+                $cur2 = $cur2->getNext();
+            }
+
+        }
+
+        $lastDifferentNode = null;
+        // 出栈，遇到第一个不相等的结点A时停止遍历，A的下一个结点B就是第一个公共结点
+        while((!$stack1->isEmpty() && $top1 = $stack1->pop()) && (!$stack2->isEmpty() && $top2 = $stack2->pop())){
+            if(!$this->equal($top1, $top2)){
+                $lastDifferentNode = $top1;
+                break;
+            }
+        }
+
+        if(is_null($lastDifferentNode)){
+            if(!$stack1->isEmpty()){
+                $lastDifferentNode = $top1;
+            }
+
+            if(!$stack2->isEmpty()){
+                $lastDifferentNode = $top2;
+            }
+        }
+
+        if(is_null($lastDifferentNode)){
+            return null;
+        }
+
+        $firstCommonNode = $lastDifferentNode->getNext();
+
+        return $firstCommonNode;
+    }
+
+    private function equal(ListNode $listNode1, ListNode $listNode2): bool
+    {
+        return ($listNode1->getVal() == $listNode2->getVal() && $listNode1->getNext() == $listNode2->getNext());
+    }
 }
 
 
@@ -84,10 +141,15 @@ $node2 = new ListNode(2);
 $node1->setNext($node2);
 $node3 = new ListNode(3);
 $node2->setNext($node3);
+$node4 = new ListNode(7);
+$node3->setNext($node4);
 
 $node21 = new ListNode(3);
-$node22 = new ListNode(9);
+$node22 = new ListNode(7);
 $node21->setNext($node22);
 
 $commonNode = $class->run1($node1, $node21);
+var_dump($commonNode);
+
+$commonNode = $class->run2($node1, $node21);
 var_dump($commonNode);
