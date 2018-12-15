006cc4e451dbd8c7f8a9fef227cae248965ab032
cg <chuganghong@qq.com>
2018-12-15 16:51:20 +0800

    请找出数组中任意一个重复的数字 easy

---
+++ Offer/Duplicate.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Offer;
+
+/**
+ * 在一个长度为n的数组里的所有数字都在0到n-1的范围内。 数组中某些数字是重复的，但不知道有几个数字是重复的。也不知道每个数字重复几次。
+ * 请找出数组中任意一个重复的数字。 例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出是第一个重复的数字2。
+ * Class Duplicate
+ * @package App\Offer
+ */
+class Duplicate
+{
+    public function run1(array $data): int
+    {
+        $count = count($data);
+        for($i = 0; $i < $count; $i++){
+            for($j = $i + 1; $j < $count && isset($data[$j]); $j++){
+                if($data[$i] == $data[$j]){
+                    return $data[$i];
+                }
+            }
+
+        }
+
+        return -1;
+    }
+}
+
+// test
+$class = new Duplicate();
+$data = [2,3,1,0,2,5,3];
+$res = $class->run1($data);
+var_dump($res);
